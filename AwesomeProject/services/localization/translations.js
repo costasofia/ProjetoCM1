45c448d6105c5e1e570e22fd4864891208842173
sofia99costa@gmail.com <sofia99costa@gmail.com>
2020-06-22 22:04:27 +0100

    multi-lingua

---
+++ AwesomeProject/services/localization/translations.js
@@ -0,0 +1,11 @@
+import LocalizedStrings from 'react-native-localization';
+export const DEFAULT_LANGUAGE = 'en';
+
+const translations = {
+    en: {
+    },
+    pt: {
+    }
+};
+
+export default new LocalizedStrings(translations);
